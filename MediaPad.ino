bf7d3bdb32f1750751b992b6415a21162916cdde
Eric Nelson <enelson@eligoenergy.com>
2015-10-26 15:56:29 -0500

    moved keys around since I broke a switch

--- MediaPad.ino
+++ MediaPad.ino
@@ -10,22 +10,22 @@
 
 #include <Keypad.h>
 
-#define KEY_MEDIA_VOLUME_INC    0x01
-#define KEY_MEDIA_VOLUME_DEC    0x02
-#define KEY_MEDIA_MUTE          0x04
-#define KEY_MEDIA_PLAY_PAUSE    0x08
-#define KEY_MEDIA_NEXT_TRACK    0x10
-#define KEY_MEDIA_PREV_TRACK    0x20
-#define KEY_MEDIA_STOP          0x40
-#define KEY_MEDIA_EJECT         0x80
+// #define KEY_MEDIA_VOLUME_INC    0x01
+// #define KEY_MEDIA_VOLUME_DEC    0x02
+// #define KEY_MEDIA_MUTE          0x04
+// #define KEY_MEDIA_PLAY_PAUSE    0x08
+// #define KEY_MEDIA_NEXT_TRACK    0x10
+// #define KEY_MEDIA_PREV_TRACK    0x20
+// #define KEY_MEDIA_STOP          0x40
+// #define KEY_MEDIA_EJECT         0x80
 
 
 const byte ROWS = 3; //four rows
 const byte COLS = 2; //three columns
 char keys[ROWS][COLS] = {
-  {KEY_MEDIA_NEXT_TRACK, KEY_MEDIA_VOLUME_INC},
-  {KEY_MEDIA_PLAY_PAUSE, KEY_MEDIA_VOLUME_DEC},
-  {KEY_MEDIA_PREV_TRACK, KEY_MEDIA_MUTE}
+  {KEY_MEDIA_PREV_TRACK, KEY_MEDIA_VOLUME_INC},
+  {KEY_MEDIA_NEXT_TRACK, KEY_MEDIA_VOLUME_DEC},
+  {KEY_MEDIA_PLAY_PAUSE, KEY_MEDIA_MUTE}
 };
 byte rowPins[ROWS] = {PIN_D0, PIN_D1, PIN_D2}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {PIN_B0, PIN_B1}; //connect to the column pinouts of the keypad
@@ -38,6 +38,12 @@ void setup() {
   Serial.begin(9600);
   kpd.setDebounceTime(1);
   Keyboard.begin();
+  Serial.println(KEY_MEDIA_PREV_TRACK);
+  Serial.println(KEY_MEDIA_PLAY_PAUSE);
+  Serial.println(KEY_MEDIA_NEXT_TRACK);
+  Serial.println(KEY_MEDIA_MUTE);
+  Serial.println(KEY_MEDIA_VOLUME_DEC);
+  Serial.println(KEY_MEDIA_VOLUME_INC);
 }
 
 unsigned long loopCount = 0;
@@ -48,7 +54,6 @@ void loop() {
 
   loopCount++;
   if ( (millis()-startTime)>1000 ) {
-      Serial.println(loopCount);
       startTime = millis();
       loopCount = 0;
   }
@@ -65,7 +70,8 @@ void loop() {
         media |= kpd.key[i].kchar;
       }
     }
+    Serial.println(media);
+    Keyboard.set_media(media);
+    Keyboard.send_now();
   }
-  Keyboard.set_media(media);
-  Keyboard.send_now();
 }  // End loop
